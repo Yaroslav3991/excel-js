022e21838d31bd588807ebd31c9f3995fedbdf4c
yaroslav3991 <yaroslav3991@gmail.com>
2020-06-23 19:10:56 +0300

    Finish base table logic

--- src/component/excel/Excel.js
+++ src/component/excel/Excel.js
@@ -1,17 +1,23 @@
 import { $ } from "@core/dom";
+import { Emitter } from "../../core/Emitter";
 
 export class Excel {
   constructor(selector, options) {
     this.$element = $(selector);
     this.components = options.components || [];
+    this.emitter = new Emitter();
   }
 
   getRoot() {
     const root = $.create("div", "excel");
 
+    const componentOptions = {
+      emitter: this.emitter,
+    };
+
     this.components = this.components.map((Component) => {
       const element = $.create("div", Component.className);
-      const component = new Component(element);
+      const component = new Component(element, componentOptions);
 
       element.html(component.toHTML());
       root.append(element);
@@ -26,5 +32,9 @@ export class Excel {
     this.$element.append(this.getRoot());
 
     this.components.forEach((component) => component.init());
+  }
+
+  unMount() {
+    this.components.forEach((component) => component.unMount());
   }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,17 +4,33 @@ export class ExcelComponent extends DomListener {
   constructor(root, options = {}) {
     super(root, options.listeners);
     this.name = options.name || "";
+    this.emitter = options.emitter;
+    this.unsubscribers = [];
+
+    this.prepare();
   }
+
+  prepare() {}
 
   toHTML() {
     return "";
+  }
+
+  $emit(eventName, ...args) {
+    this.emitter.emit(eventName, ...args);
+  }
+
+  $on(eventName, callback) {
+    const unsub = this.emitter.subscribe(eventName, callback);
+    this.unsubscribers.push(unsub);
   }
 
   init() {
     this.initDOMListeners();
   }
 
-  remove() {
+  unMount() {
     this.removeDOMListeners();
+    this.unsubscribers.forEach((unsub) => unsub());
   }
 }
